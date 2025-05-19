ea590a3f5b5f5b599013fa94e65272726ee9bcce
Ayilavan <teamayilavan@gmail.com>
2025-05-19 16:00:41 +0530

    beautified header and footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,100 @@
-<footer>
-    <p>Team Ayilavan</p>
+<script lang="ts">
+	import logo from '$lib/assets/Ayilavan_Logo.svg';
+</script>
+
+<footer
+	class="border-highlight-text mt-12 flex flex-col items-center justify-between gap-5 border-t-1 p-4 lg:flex-row"
+>
+	<div class="social text-text-gray flex gap-2">
+		<a
+			class="hover:text-highlight-text"
+			href="https://www.youtube.com/@teamayilavan"
+			target="_blank"
+			aria-label="Youtube Link"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="32"
+				height="32"
+				fill="currentColor"
+				viewBox="0 0 256 256"
+			>
+				<path
+					d="M164.44,121.34l-48-32A8,8,0,0,0,104,96v64a8,8,0,0,0,12.44,6.66l48-32a8,8,0,0,0,0-13.32ZM120,145.05V111l25.58,17ZM234.33,69.52a24,24,0,0,0-14.49-16.4C185.56,39.88,131,40,128,40s-57.56-.12-91.84,13.12a24,24,0,0,0-14.49,16.4C19.08,79.5,16,97.74,16,128s3.08,48.5,5.67,58.48a24,24,0,0,0,14.49,16.41C69,215.56,120.4,216,127.34,216h1.32c6.94,0,58.37-.44,91.18-13.11a24,24,0,0,0,14.49-16.41c2.59-10,5.67-28.22,5.67-58.48S236.92,79.5,234.33,69.52Zm-15.49,113a8,8,0,0,1-4.77,5.49c-31.65,12.22-85.48,12-86,12H128c-.54,0-54.33.2-86-12a8,8,0,0,1-4.77-5.49C34.8,173.39,32,156.57,32,128s2.8-45.39,5.16-54.47A8,8,0,0,1,41.93,68c30.52-11.79,81.66-12,85.85-12h.27c.54,0,54.38-.18,86,12a8,8,0,0,1,4.77,5.49C221.2,82.61,224,99.43,224,128S221.2,173.39,218.84,182.47Z"
+				>
+				</path>
+			</svg>
+		</a>
+		<a
+			class="hover:text-highlight-text"
+			href="https://www.instagram.com/teamayilavan"
+			target="_blank"
+			aria-label="Instagram Link"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="32"
+				height="32"
+				fill="currentColor"
+				viewBox="0 0 256 256"
+			>
+				<path
+					d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160ZM176,24H80A56.06,56.06,0,0,0,24,80v96a56.06,56.06,0,0,0,56,56h96a56.06,56.06,0,0,0,56-56V80A56.06,56.06,0,0,0,176,24Zm40,152a40,40,0,0,1-40,40H80a40,40,0,0,1-40-40V80A40,40,0,0,1,80,40h96a40,40,0,0,1,40,40ZM192,76a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z"
+				>
+				</path>
+			</svg>
+		</a>
+		<a
+			class="hover:text-highlight-text"
+			href="https://www.facebook.com/profile.php?id=61576495169699"
+			target="_blank"
+			aria-label="Facebook Link"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="32"
+				height="32"
+				fill="currentColor"
+				viewBox="0 0 256 256"
+			>
+				<path
+					d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm8,191.63V152h24a8,8,0,0,0,0-16H136V112a16,16,0,0,1,16-16h16a8,8,0,0,0,0-16H152a32,32,0,0,0-32,32v24H96a8,8,0,0,0,0,16h24v63.63a88,88,0,1,1,16,0Z"
+				>
+				</path>
+			</svg>
+		</a>
+
+		<a
+			class="hover:text-highlight-text"
+			href="https://x.com/teamayilavan"
+			target="_blank"
+			aria-label="X Link"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="32"
+				height="32"
+				fill="currentColor"
+				viewBox="0 0 256 256"
+			>
+				<path
+					d="M214.75,211.71l-62.6-98.38,61.77-67.95a8,8,0,0,0-11.84-10.76L143.24,99.34,102.75,35.71A8,8,0,0,0,96,32H48a8,8,0,0,0-6.75,12.3l62.6,98.37-61.77,68a8,8,0,1,0,11.84,10.76l58.84-64.72,40.49,63.63A8,8,0,0,0,160,224h48a8,8,0,0,0,6.75-12.29ZM164.39,208,62.57,48h29L193.43,208Z"
+				>
+				</path>
+			</svg>
+		</a>
+	</div>
+
+	<a href="https://www.ayilavan.org" target="_blank">
+		<div class="w-36 p-2">
+			<img src={logo} alt="Ayilavan Logo" />
+		</div>
+	</a>
+
+	<div class="copyright items-center text-center lg:text-left">
+		<p class="text-sm">&copy; 2025, Team Ayilavan</p>
+		<a href="terms"><small>Terms and Conditions</small></a>
+		<span class="text-text-gray">|</span>
+		<a href="privacy"><small>Privacy Policy</small></a>
+	</div>
 </footer>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,10 @@
-<header class="flex h-6 bg-green-800">
-<p>Easy Thiruppugazh</p>
+<script lang="ts">
+	import logo from '$lib/assets/music.svg';
+</script>
+
+<header class="bg-background border-highlight-text flex border-b-1 py-4">
+	<a class="flex items-center mx-auto" href="/">
+		<img class="h-14" src={logo} alt="Music Logo" />
+		<p class="font-medium text-text-body w-48">எளிய திருப்புகழ் பாடல்கள் தொகுப்பு</p>
+	</a>
 </header>
