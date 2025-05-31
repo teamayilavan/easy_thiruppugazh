a0a69c2b3dd6387caccfe7f5d9c40c212e48ba8f
Ayilavan <teamayilavan@gmail.com>
2025-05-31 10:42:29 +0530

    added terms and conditions, privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,116 @@
+<svelte:head>
+	<title>எளிய திருப்புகழ் பாடல்கள் தொகுப்பு</title>
+	<meta
+		name="description"
+		content="This page contains teh privacy policy for the Eliya Thiruppugazh Paadagal Collection website."
+	/>
+	<meta
+		name="keywords"
+		content={'திருப்புகழ், அருணகிரிநாத சுவாமிகள்,முருக பெருமான், வெற்றிவேல், சிறப்பு'}
+	/>
+	<meta name="author" content="அயிலவன் அணி" />
+</svelte:head>
+
+<div class="mt-12">
+	<h1 class="noto text-text-high dark:text-card text-center text-2xl font-semibold">
+		தனியுரிமைக் கொள்கை
+	</h1>
+
+	<p class="mt-4 text-center">கடைசியாகப் புதுப்பிக்கப்பட்டது: மே 31, 2025</p>
+
+	<p class="mt-4">
+		எங்கள் வலைத்தளத்தைப் பார்வையிட்டதற்கு நன்றி. உங்கள் தனியுரிமையைப் பாதுகாக்க நாங்கள்
+		கடமைப்பட்டுள்ளோம்.
+	</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">நாங்கள் சேகரிக்காத தகவல்கள்</h2>
+	<p>Wஎங்கள் பார்வையாளர்களிடமிருந்து நாங்கள் எந்த தனிப்பட்ட தகவலையும் சேகரிப்பதில்லை.</p>
+	<p>நீங்கள் எங்கள் வலைத்தளத்தை அநாமதேயமாக உலாவலாம்.</p>
+	<p>உங்களைக் கண்காணிக்க நாங்கள் குக்கீகளைப் பயன்படுத்துவதில்லை</p>
+	<p>
+		உங்களை தனிப்பட்ட முறையில் அடையாளம் காணும் வேறு எந்த கண்காணிப்பு தொழில்நுட்பங்களையும் நாங்கள்
+		பயன்படுத்துவதில்லை
+	</p>
+	<p>எங்கள் வலைத்தளத்தைப் பயன்படுத்த நாங்கள் உங்களிடம் எந்த தனிப்பட்ட தகவலையும் கேட்கவில்லை</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">இணையதள பகுப்பாய்வு</h2>
+
+	<p>
+		எங்கள் வலைத்தள траஃபிக்கைப் புரிந்துகொள்ளவும், எங்கள் வலைத்தளத்தை மேம்படுத்தவும் நாங்கள்
+		பிளாசிபில் அனலிட்டிக்ஸ் (Plausible Analytics) ஐப் பயன்படுத்துகிறோம். பிளாசிபில் அனலிட்டிக்ஸ்
+		என்பது தனியுரிமையை மையமாகக் கொண்ட ஒரு பகுப்பாய்வுக் கருவியாகும், இது குக்கீகளைப் பயன்படுத்தாது
+		மற்றும் எந்தவொரு தனிப்பட்ட தரவையோ அல்லது தனிப்பட்ட முறையில் அடையாளம் காணக்கூடிய தகவலையோ
+		சேகரிக்காது. பிளாசிபில் அனலிட்டிக்ஸ் மூலம் சேகரிக்கப்பட்ட அனைத்து தரவுகளும் ஒருங்கிணைக்கப்பட்டு
+		அநாமதேயமாக்கப்படுகின்றன. பிளாசிபில் அனலிட்டிக்ஸ் மற்றும் அவர்களின் தரவு நடைமுறைகளைப் பற்றி
+		நீங்கள் இங்கே மேலும் அறியலாம்:
+	</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">உங்கள் தனியுரிமை</h2>
+
+	<p>
+		நாங்கள் உங்கள் தனிப்பட்ட தகவல்களைச் சேகரிக்காததால், எங்கள் வலைத்தளத்திற்கான உங்கள் வருகை தனிப்பட்டதாகவே இருக்கும்.
+	</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">இந்த தனியுரிமைக் கொள்கையில் மாற்றங்கள்
+
+</h2>
+
+	<p>
+		இந்த தனியுரிமைக் கொள்கையை நாங்கள் அவ்வப்போது புதுப்பிக்கலாம். ஏதேனும் மாற்றங்கள் இந்தப் பக்கத்தில் புதுப்பிக்கப்பட்ட "கடைசியாகப் புதுப்பிக்கப்பட்டது" தேதியுடன் வெளியிடப்படும்.
+	</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">தொடர்புக்கு</h2>
+
+	<p>
+		இந்த தனியுரிமைக் கொள்கையைப் பற்றி உங்களுக்கு ஏதேனும் கேள்விகள் இருந்தால், நீங்கள் எங்களை இங்கே தொடர்பு கொள்ளலாம்:: contact@ayilavan.org
+	</p>
+</div>
+
+<div class="mt-12">
+	<h1 class="noto text-text-high dark:text-card text-center text-2xl font-semibold">
+		Privacy Policy
+	</h1>
+
+	<p class="mt-4">Last Updated: May 31, 2025</p>
+
+	<p class="mt-4">
+		Thank you for visiting our website. We are committed to protecting your privacy.
+	</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">Information We Do Not Collect</h2>
+	<p>We do not collect any personal information from our visitors.</p>
+	<p>You can browse our website anonymously</p>
+	<p>We do not use cookies to track you</p>
+	<p>We do not use any other tracking technologies that identify you personally</p>
+	<p>We do not ask you to provide any personal information to use our website</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">Website Analytics</h2>
+
+	<p>
+		We use Plausible Analytics to help us understand our website traffic and improve our website.
+		Plausible Analytics is a privacy-focused analytics tool that does not use cookies and does not
+		collect any personal data or personally identifiable information. All data collected by
+		Plausible Analytics is aggregated and anonymized. You can learn more about Plausible Analytics
+		and their data practices here:
+	</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">Your Privacy</h2>
+
+	<p>
+		Because we do not collect any of your personal information, your visit to our website remains
+		private.
+	</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">Changes to This Privacy Policy</h2>
+
+	<p>
+		We may update this Privacy Policy from time to time. Any changes will be posted on this page
+		with an updated "Last Updated" date
+	</p>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">Contact Us</h2>
+
+	<p>
+		If you have any questions about this Privacy Policy, you can contact us at: contact@ayilavan.org
+	</p>
+</div>
---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,68 @@
+<svelte:head>
+	<title>எளிய திருப்புகழ் பாடல்கள் தொகுப்பு</title>
+	<meta
+		name="description"
+		content="This page contains teh copyright policy for the Eliya Thiruppugazh Paadagal Collection website."
+	/>
+	<meta
+		name="keywords"
+		content={'திருப்புகழ், அருணகிரிநாத சுவாமிகள்,முருக பெருமான், வெற்றிவேல், சிறப்பு'}
+	/>
+	<meta name="author" content="அயிலவன் அணி" />
+</svelte:head>
+
+<div class="mt-12">
+	<h1 class="noto text-text-high dark:text-card text-center text-2xl font-semibold">
+		எழுத்து உரிமைகள்
+	</h1>
+	<div class="mt-4">
+		<p>1. இந்த தலத்தில் வரும் திருப்புகழ் பாடல்கள் மூலம் நாட்டுடமை ஆக்க பட்டது</p>
+		<p>2. இதில் வரும் சொற்பொருள் நிகண்டுகள் கொண்டு எழுதப்பட்டது</p>
+		<p>3. பதம் பிரித்து கொடுத்தது எங்கள் முயற்சி</p>
+		<p>4. பொருளுரை கௌமாரம் தலத்தில் இருந்து எடுக்கப்பட்டது</p>
+		<p>5. விளக்கவுரை எங்களுடைய படைப்பு</p>
+		<p>6. இதனை புத்தக வடிவில் செய்தது எங்கள் படைப்பு</p>
+	</div>
+	<h2 class="mt-12 mb-2 text-center text-lg font-bold">
+		இந்த தலத்தில் உள்ள செய்திகளை நீங்கள் பொதுகாரியங்களுக்கு தாராளமாக பயன்படுத்தலாம். உரிமை கோர
+		வேண்டிய அவசியம் இல்லை.
+	</h2>
+
+	<h2 class="mt-4 mb-2 text-center text-lg font-bold">
+		PDF வடிவில் தரவிறக்கம் செய்து, அச்சிட்டு விற்க வேண்டாம் என்று கேட்டு கொள்கிறோம்
+	</h2>
+
+	<h1 class="noto text-text-high dark:text-card mt-12 text-center text-2xl font-semibold">இசை</h1>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold">
+		இந்த தளத்தில் வரும் அத்துணை இசை எங்களின் படைப்பு. தயவு செய்து எங்களிடம் கேட்டு இதனை நீங்கள்
+		பயன்படுத்துமாறு வேண்டுகிறோம்
+	</h2>
+</div>
+
+<div class="mt-12">
+	<h1 class="noto text-text-high dark:text-card text-center text-2xl font-semibold">
+		Literary Rights
+	</h1>
+	<div class="mt-4">
+		<p>1. The Thiruppugazh songs in this website are nationalized</p>
+		<p>2. Word meaning are taken from different tamil dictionaries</p>
+		<p>3. Word segmentation is our effort</p>
+		<p>4. Meaning is taken from Kaumaram website</p>
+		<p>5. Longer explanations are our effort</p>
+		<p>6. PDF format is our effort</p>
+	</div>
+	<h2 class="mt-12 mb-2 text-center text-lg font-bold">
+		You can use this website's content for public purposes freely. No need to ask for rights.
+	</h2>
+
+	<h2 class="mt-4 mb-2 text-center text-lg font-bold">
+		Kindly do not print and sell the PDF versions.
+	</h2>
+
+	<h1 class="noto text-text-high dark:text-card mt-12 text-center text-2xl font-semibold">Music</h1>
+
+	<h2 class="mt-4 mb-2 text-lg font-bold text-center">
+		All the music in this website is our creation. Please ask us before using it.
+	</h2>
+</div>
