7bcc2ea958f4e49458e40cd0af5bf0d1592ca978
Ayilavan <teamayilavan@gmail.com>
2025-05-31 10:50:47 +0530

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,30 @@
+
+<div class="text-center mx-auto mt-12 py-12">
+    <h1 class="text-5xl text-text-high dark:text-dtext-high my-4 font-bold">404</h1>
+<p>மன்னிக்கவும். நீங்கள் கேட்ட பக்கத்தை காண்பிக்க இயலவில்லை
+</p>
+<p>தயவுசெய்து வலைப்பாதையை சரிபார்க்கவும்</p>
+</div>
+
+<a class="bg-accent hover:bg-dcard mx-auto mt-12 block w-72 rounded-md px-4 py-2" href="/">
+	<div class="flex align-middle gap-2 items-center">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="lucide lucide-house-icon lucide-house"
+			><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" /><path
+				d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"
+			/></svg
+		>
+		முகப்பு பக்கம் செல்லவும்
+	</div></a
+>
+
+
